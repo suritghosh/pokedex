c85924d07c5467c17dca19da56e6478af889a2b6
suritghosh <suritghosh45@gmail.com>
2022-04-14 23:42:14 +0530

    Complete

---
+++ pokedex/src/app/details/details.page.ts
@@ -0,0 +1,30 @@
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  details: any;
+  slideOpts =  {
+    // autoplay: {
+    //   delay: 1000,
+    //   disableOnInteraction: false
+    // }
+  };
+
+  constructor(private route: ActivatedRoute, private pokeService: PokemonService) { }
+
+  ngOnInit() {
+    let index = this.route.snapshot.paramMap.get('index');
+    this.pokeService.getPokeDetails(index).subscribe(details => {
+      console.log('Details: ', details);
+      this.details = details;
+    })
+  }
+
+}
---
+++ pokedex/src/app/home/home.page.ts
@@ -0,0 +1,55 @@
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  offset = 0;
+  pokemon = [];
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+
+  constructor(private pokeService: PokemonService) {}
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      this.pokemon = [...this.pokemon, ...res];
+
+      if (event) {
+        event.target.complete();
+      }
+
+      if (this.offset == 125) {
+        this.infinite.disabled = true;
+      }
+    });
+  }
+
+  onSearchChange(e) {
+    let value = e.detail.value;
+
+    if (value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    });
+  }
+
+}
